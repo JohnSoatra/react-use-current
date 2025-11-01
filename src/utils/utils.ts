1a276cedfe8d5f88d26fe8158e0118cfcbdc6524
John Soatra <johnsoatra@gmail.com>
2025-11-01 19:40:00 +0700

    Add new utils isArray isProxy

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,19 +1,53 @@
-import { MutationMethods } from "../constants";
-import Labels from "../constants/labels";
+import createProxy, { CacheProxy, CacheShallow, ReRender } from "./createProxy";
+import Symbols from "../constants/symbols";
+import MutationMethods from "../constants/mutationMethods";
 
 export function creatable(value: any) {
   return typeof value === 'object' && value !== null;
 }
 
-export function iterable(value: any) {
-  return value != null && typeof value[Symbol.iterator] === 'function';
+export function isArray(value: any): boolean {
+  return Array.isArray(value) || ArrayBuffer.isView(value);
 }
 
-export function isProxy(value: any) {
-  return value[Labels.IsProxy] ?? false;
+export function isProxy(value: any): boolean {
+  return value[Symbols.IsProxy] ?? false;
+}
+
+export function getRaw(proxy: any): object | undefined {
+  return proxy[Symbols.RawObject];
 }
 
 export function mutationMethod(obj: any, key: string) {
   const list = MutationMethods.get(obj.constructor);
   return list ? list.includes(key) : false;
+}
+
+export function toProxies(
+  reRender: ReRender,
+  cacheProxy: CacheProxy,
+  cacheShallow: CacheShallow,
+  ...args: any[]
+) {
+  let array:any[] = [];
+
+  for (const each of args) {
+    let result;
+
+    if (creatable(each)) {
+      if (isProxy(each)) {
+        result = each;
+      } else if (cacheProxy.has(each)) {
+        result = cacheProxy.get(each);
+      } else {
+        result = createProxy(each, reRender, cacheProxy, cacheShallow);
+      }
+    } else {
+      result = each;
+    }
+
+    array.push(result);
+  }
+
+  return array;
 }
