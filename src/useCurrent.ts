1d96b42035f3059b673eece913206e09717ea828
John Soatra <johnsoatra@gmail.com>
2025-11-01 19:39:22 +0700

    Change signal type {} to symbol

--- src/useCurrent.ts
+++ src/useCurrent.ts
@@ -1,29 +1,29 @@
 import { useCallback, useMemo, useState } from 'react';
-import createProxy from './utils/createProxy';
+import createProxy, { CacheProxy, CacheShallow } from './utils/createProxy';
 
 export type Current<T> = {
   get current(): T;
   set current(value: T);
-  readonly signal: {} | undefined;
+  readonly signal: Symbol | undefined;
 };
 
 function useCurrent<T>(initial: T): Current<T>;
 function useCurrent<T = undefined>(): Current<T | undefined>;
 function useCurrent<T>(initial?: T): Current<T | undefined> {
-  const [signal, setSignal] = useState<{}>();
+  const [signal, setSignal] = useState<Symbol>();
   const packet = useMemo(() => ({ current: initial, signal }), []);
-  const cache = useMemo(() => new WeakMap(), []);
-  const cacheArray = useMemo(() => new WeakMap(), []);
+  const cacheProxy = useMemo<CacheProxy>(() => new WeakMap(), []);
+  const cacheShallow = useMemo<CacheShallow>(() => new WeakMap(), []);
 
   const reRender = useCallback(() => {
     setSignal(() => {
-      const newSignal = Object.freeze({});
+      const newSignal = Symbol();
       packet.signal = newSignal;
       return newSignal;
     });
   }, []);
 
-  return useMemo(() => createProxy(packet, reRender, cache, cacheArray), []);
+  return useMemo(() => createProxy(packet, reRender, cacheProxy, cacheShallow), []);
 }
 
 export default useCurrent;
